e2882df921c476f7acaaae321bf1098e18c44acf
Vighnesh Ramasamy <vighneshramasamy@gmail.com>
2025-03-23 13:53:11 +0800

    updated q1 dag file

--- AirflowScripts/q1/emr-dag-q1.py
+++ AirflowScripts/q1/emr-dag-q1.py
@@ -10,6 +10,7 @@ from airflow.providers.amazon.aws.operators.emr import (
 )
 from airflow.providers.amazon.aws.sensors.emr import EmrStepSensor
 from airflow.providers.amazon.aws.operators.glue_crawler import GlueCrawlerOperator
+from airflow.providers.amazon.aws.operators.athena import AthenaOperator
 
 # --------------------------------------
 # Set default arguments
@@ -49,14 +50,14 @@ JOB_FLOW_OVERRIDES = {
         {'Name': 'JupyterEnterpriseGateway'},
         {'Name': 'Hue'},
     ],
-    'BootstrapActions': [
-        {
-            'Name': 'Install Dependencies',
-            'ScriptBootstrapAction': {
-                'Path': 's3://is459-g1t7-smart-meters-in-london/bootstrap/emr-setup-q1.sh', # [TO DO] upload .sh config file
-            }
-        },
-    ],
+    # 'BootstrapActions': [
+    #     {
+    #         'Name': 'Install Dependencies',
+    #         'ScriptBootstrapAction': {
+    #             'Path': 's3://is459-g1t7-smart-meters-in-london/bootstrap/emr-setup-q1.sh', # [TO DO] upload .sh config file
+    #         }
+    #     },
+    # ],
     'Instances': {
         'InstanceGroups': [
             {
@@ -118,7 +119,7 @@ etl_step_adder = EmrAddStepsOperator(
                     'spark-submit',
                     '--deploy-mode', 'cluster',
                     '--master', 'yarn',
-                    "s3://is459-g1t7-smart-meters-in-london/pyspark-scripts/q1-etl-pyspark.py", # [TO DO] upload 1-etl-pyspark.py
+                    "s3://is459-g1t7-smart-meters-in-london/pyspark-scripts/q1-etl-pyspark-v2.py", # 1. CHANGE TO LATEST SPARK CODE
                 ],
             },
         }
@@ -145,18 +146,50 @@ glue_crawler_task = GlueCrawlerOperator(
     task_id='run_glue_crawler_q1',
     config = {
         'Name': 'glue_crawler_output_q1',
-        'Role': 'arn:aws:iam::761018854594:role/service-role/AWSGlueServiceRole-project-q2-real',  # [TO DO] create role
-        'DatabaseName': 'mwaa-output-database',  # [TO DO] change database name
+        'Role': 'arn:aws:iam::761018854594:role/AWSGlueServiceRole-project-q1-v1',  # [TO DO] create role
+        'DatabaseName': 'q1-processed-data-schema',  # [TO DO] change database name
         'Targets': {
-            'S3Targets': [
+            'S3Targets': [                                                                                  # 2. UPDATE TARGET TO PROCESSED DATA FOLDERS
+                # {
+                #     'Path': 's3://is459-g1t7-smart-meters-in-london/processed-data/merged_df12_acorn/',  # [TO DO] change file name
+                #     'Exclusions': [],
+                #     'SampleSize': 2,
+                # },
+                # {
+                #     'Path': 's3://is459-g1t7-smart-meters-in-london/processed-data/merged_df12_acorn_grouped/',  # [TO DO] change file name
+                #     'Exclusions': [],
+                #     'SampleSize': 2,
+                # },
+                # {
+                #     'Path': 's3://is459-g1t7-smart-meters-in-london/processed-data/merged_df12_acorn_category/',  # [TO DO] change file name
+                #     'Exclusions': [],
+                #     'SampleSize': 2,
+                # },
                 {
-                    'Path': 's3://is459-g1t7-smart-meters-in-london/mwaa-output/merged_df1_df3_df7_df8/',  # [TO DO] change file name
+                    'Path': 's3://is459-g1t7-smart-meters-in-london/processed-data/merged_df1_df5_df10/',  # for OLD PYSPARK CODE
                     'Exclusions': [],
                     'SampleSize': 2,
                 },
             ],
         },
     },
+    aws_conn_id='aws_default',
+    dag=dag,
+)
+
+# --------------------------------------
+# Add Athena Query Task to DAG
+# --------------------------------------
+athena_query_task = AthenaOperator(
+    task_id='execute_athena_query_q1',          # 3. UPDATE QUERY TO RETRIEVE FROM CORRECT SCHEMA (CHANGE merged_df1_df5_df10 TO CORRECT ONE)
+    query="""
+        SELECT 
+            *
+        FROM "q1-processed-data-schema"."merged_df1_df5_df10"
+    """,
+    database='"q1-processed-data-schema"',  # Ensure this matches the Glue Crawler output database
+    output_location='s3://is459-g1t7-smart-meters-in-london/athena-results/',  # Change to a valid S3 bucket
+    workgroup='primary',
     aws_conn_id='aws_default',
     dag=dag,
 )
@@ -174,4 +207,4 @@ cluster_terminator = EmrTerminateJobFlowOperator(
 # --------------------------------------
 # Define DAG Dependency
 # --------------------------------------
-cluster_creator >> etl_step_adder >> etl_step_checker >> glue_crawler_task >> cluster_terminator
+cluster_creator >> etl_step_adder >> etl_step_checker >> glue_crawler_task >> athena_query_task >> cluster_terminator
