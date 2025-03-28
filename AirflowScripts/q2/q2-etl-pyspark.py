c100badc406549f5694f59166bbb8a33c1095f24
kohchloe <chloekoh.2021@scis.smu.edu.sg>
2025-03-28 13:16:54 +0800

    update ETL script for q2

--- AirflowScripts/q2/q2-etl-pyspark.py
+++ AirflowScripts/q2/q2-etl-pyspark.py
@@ -1,24 +1,15 @@
 # Datasets needed:
-# df1 daily_dataset.csv
-# df2 halfhourly_dataset.csv (greater granularity)
-# df3 hhblock_dataset.zip (block files of transposed data of a household's energy 
-# consumption in a day; hh_0 column = consumption from 00:00 - 00:30, and so on)
+# df1 daily_dataset.zip (not csv) - block files of a household's daily energy consumption
 # df7 uk_bank_holidays.csv 
 # df8 weather_daily_darksky.csv
-# df9 weather_hourly_darksky.csv (greater granularity)
-
 # Output: merged_q2_daily (Parquet format) -> df1 + df8 + df7
-#         merged_q2_hourly (Parquet format) -> df2 + df9 + df7
 
-# import sys
-# from datetime import datetime
+# Archived datasets and reasons for not using them:
+# df3 hhblock_dataset.zip (hh_0 column = consumption from 00:00 - 00:30, and so on) - overlaps with df1
+# df2 halfhourly_dataset.csv - replaced with df3 as data is too granular and not needed for this analysis
+# df9 weather_hourly_darksky.csv - replaced with df8 as data is too granular and not needed for this analysis
 
 from pyspark.sql import SparkSession
-# from pyspark.sql.functions import (
-#     col, when, to_date, date_format,
-#     dayofweek, month, hour, lag, avg,
-#     sum as spark_sum
-# )
 from pyspark.sql.functions import *
 from pyspark.sql.window import Window
 
@@ -34,18 +25,12 @@ def main():
         .getOrCreate()
 
     # Read existing CSV datasets from S3
-    df1 = spark.read.csv(f"{S3_INPUT_FOLDER}daily_dataset.csv", header=True, inferSchema=True)
-    df2 = spark.read.csv(f"{S3_INPUT_FOLDER}halfhourly_dataset.csv", header=True, inferSchema=True)
+    df1 = spark.read.csv(f"{S3_INPUT_FOLDER}daily_dataset.csv", header=True, inferSchema=True) # df1's zipped files are merged in q2-merge-daily-data.py
     df7 = spark.read.csv(f"{S3_INPUT_FOLDER}uk_bank_holidays.csv", header=True, inferSchema=True)
     df8 = spark.read.csv(f"{S3_INPUT_FOLDER}weather_daily_darksky.csv", header=True, inferSchema=True)
-    df9 = spark.read.csv(f"{S3_INPUT_FOLDER}weather_hourly_darksky.csv", header=True, inferSchema=True)
-
-    # Read existing zip datasets from S3
-    # hhblock_path = "s3://is459-g1t7-smart-meters-in-london/raw-data/hhblock_dataset/block_*.csv"
-    # df3 = spark.read.csv(hhblock_path, header=True, inferSchema=True)
 
     # Rename df1's `day` column to `date` and convert to datetime format
-    df1 = df1.withColumnRenamed("day", "date").withColumn("date", to_date(col("date"), "dd/MM/yyyy"))
+    df1 = df1.withColumnRenamed("day", "date").withColumn("date", to_date(col("date"), "M/d/yyyy"))
 
     # Create new columns for time-based features
     df1 = (df1.withColumn("day_of_week", dayofweek(col("date")))
@@ -65,7 +50,7 @@ def main():
     })
 
     # Convert df8's `time` column to `date` and extract hour
-    df8 = (df8.withColumn("date", to_date(col("time"), "dd/MM/yyyy HH:mm"))
+    df8 = (df8.withColumn("date", to_timestamp(col("time"), "dd/MM/yyyy HH:mm").cast("date"))
             .withColumn("hour", col("time").substr(12, 2).cast("int")))
 
     # Create new columns for weather-based features
@@ -80,23 +65,29 @@ def main():
                                     .collect()[0][0])
     avg_cloud_temp_interaction = (df8.select((avg(col("cloudCover")) * avg(col("temperatureMax"))).alias("avg_cloud_temp_interaction"))
                                     .collect()[0][0])
-
-    # Rolling sum of precipitation over 7 days
-    # df8 = df8.withColumn("rolling_precipitation_7d", spark_sum("precipType").over(window_spec.rowsBetween(-6, 0)))
+    
+    # Map precipType values to integers
+    if "precipType" in df8.columns:
+        df8 = df8.withColumn(
+            "precipType",
+            when(col("precipType") == "Rain", 1)
+            .when(col("precipType") == "Snow", 2)
+            .when(col("precipType") == "Clear", 0)
+            .otherwise(0)
+        )
 
     # Fill missing values instead of dropping rows in df8
     df8 = df8.fillna({
         "temperature_variability": avg_temp_variability,
         "humidity_temp_interaction": avg_humidity_temp_interaction,
         "cloud_temp_interaction": avg_cloud_temp_interaction,
-        # "rolling_precipitation_7d": 0
     })
 
     # Merge df1 and df8 on `date` column
     merged_df1_df8 = df1.join(df8, on="date", how="left")
 
     # Rename df7's `Bank Holidays` column to `date` and convert to datetime format
-    df7 = (df7.withColumn("Bank holidays", to_date(col("Bank holidays"), "yyyy-MM-dd"))
+    df7 = (df7.withColumn("Bank holidays", to_date(col("Bank holidays"), "M/d/yyyy"))
             .withColumnRenamed("Bank holidays", "date")
             .withColumnRenamed("Type", "holiday"))
     
@@ -107,7 +98,7 @@ def main():
         how="left"
     )
 
-    # Drop columns with too many missing values
+    # Drop columns with too many missing values / irrelevant
     columns_to_drop = ["energy_std", "uvIndexTime", "moonPhase"]
     for col_name in columns_to_drop:
         if col_name in merged_daily_df1_df7_df8.columns:
