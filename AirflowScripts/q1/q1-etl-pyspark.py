029d956ec1b3f78e06e5f6455a5d4853a0993370
Shannie Kwok <101805908+shanniekwok@users.noreply.github.com>
2025-03-29 21:10:04 +0800

    update pyspark file
    Co-Authored-By: Poh Li Ying <111348120+liyingpoh@users.noreply.github.com>

--- AirflowScripts/q1/q1-etl-pyspark.py
+++ AirflowScripts/q1/q1-etl-pyspark.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, mean, to_timestamp
-# from pyspark.sql.types import DecimalType
+from pyspark.sql.window import Window
+from pyspark.sql import functions as F
+import os
 
 # S3 Paths
 S3_INPUT_FOLDER = "s3://is459-g1t7-smart-meters-in-london/raw-data/"
@@ -11,17 +13,25 @@ def main():
     spark = SparkSession.builder.appName("SmartMetersProcessingQ1").getOrCreate()
 
     # ---------- READ INPUT FILES ----------
+
     df2 = spark.read.csv(f"{S3_INPUT_FOLDER}halfhourly_dataset.csv", header=True, inferSchema=True)
+    df4 = spark.read.csv(f"{S3_INPUT_FOLDER}acorn_details.csv", header=True, inferSchema=True)
     df6 = spark.read.csv(f"{S3_INPUT_FOLDER}informations_households.csv", header=True, inferSchema=True)
     df6 = df6.drop(df6["file"])
     df10_1 = spark.read.csv(f"{S3_INPUT_FOLDER}acorn_information.csv", header=True, inferSchema=True)
     df10_reduced = df10_1.select("Acorn", "Acorn Category")
     df12 = spark.read.csv(f"{S3_INPUT_FOLDER}tariff_type.csv", header=True, inferSchema=True)
+    df14 = spark.read.csv(f"{S3_INPUT_FOLDER}property_type_energy_efficiency.csv", header=True, inferSchema=True)
 
     # ---------- CAST COLUMNS ----------
+
     df2 = df2.withColumn("LCLid", col("LCLid").cast("string"))
     df2 = df2.withColumn("tstp", col("tstp").cast("string"))
     df2 = df2.withColumn("energy(kWh/hh)", col("energy(kWh/hh)").cast("double"))
+
+    df4 = df4.withColumn("MAIN CATEGORIES", col("MAIN CATEGORIES").cast("string"))
+    df4 = df4.withColumn("CATEGORIES", col("CATEGORIES").cast("string"))
+    df4 = df4.withColumn("REFERENCE", col("REFERENCE").cast("string"))
 
     df6 = df6.withColumn("stdorToU", col("stdorToU").cast("string"))
     df6 = df6.withColumn("Acorn", col("Acorn").cast("string"))
@@ -34,10 +44,18 @@ def main():
     df12 = df12.withColumn("Tariff", col("Tariff").cast("string"))
     df12 = df12.withColumn("TariffDateTime", to_timestamp("TariffDateTime", "M/d/yy HH:mm"))
 
+    df14 = df14.withColumn("Housing Type", col("Housing Type").cast("string"))
+    df14 = df14.withColumn("Current Efficiency", col("Current Efficiency").cast("double"))
+    df14 = df14.withColumn("Potential Efficiency", col("Potential Efficiency").cast("double"))
+    df14 = df14.withColumn("Difference", col("Difference").cast("double"))
+
     print("\n----------------------------------------------------")
     print("Reading input files:")
     print(f"Input df2 columns: {df2.columns}")
     df2.show(5)
+
+    print(f"Input df4 columns: {df4.columns}")
+    df12.show(5)
     
     print(f"Input df6 columns: {df6.columns}")
     df6.show(5)
@@ -48,12 +66,74 @@ def main():
     print(f"Input df12 columns: {df12.columns}")
     df12.show(5)
 
+    print(f"Input df14 columns: {df14.columns}")
+    df12.show(5)
+
     # ---------- DROP UNNECESSARY ROWS ----------
+
     df2 = df2.na.drop(subset=["energy(kWh/hh)"])
 
+    # ---------- PROCESS ACORN DETAILS ----------
+    # Emulate melt with stack:
+    # Adjust the number of rows (N) in the stack() function based on the actual number of ACORN columns
+    print(df4.columns)
+    df4_melt = df4.selectExpr(
+        "`MAIN CATEGORIES`", "CATEGORIES", "REFERENCE",
+        "stack(9, 'ACORN-A', `ACORN-B`, 'ACORN-C', `ACORN-D`, 'ACORN-E', `ACORN-F`, " +
+        "'ACORN-G', `ACORN-H`, 'ACORN-I', `ACORN-J`, 'ACORN-K', `ACORN-K`, " +
+        "'ACORN-L', `ACORN-M`, 'ACORN-N', `ACORN-O`, 'ACORN-P', `ACORN-Q`) as (Acorn, Value)"
+    )
+
+    # Cast column types
+    df4_melt = df4_melt.withColumn("Acorn", col("Acorn").cast("string"))
+    df4_melt = df4_melt.withColumn("Value", col("Value").cast("double"))
+
+    # Calculate Total and Percentage using a window partitioned by MAIN CATEGORIES, CATEGORIES, and Acorn
+    window_spec = Window.partitionBy("MAIN CATEGORIES", "CATEGORIES", "Acorn")
+    df4_melt = df4_melt.withColumn("Total", F.sum("Value").over(window_spec))
+    df4_melt = df4_melt.withColumn("Percentage", col("Value") / col("Total"))
+
+    # Cast column types
+    df4_melt = df4_melt.withColumn("Percentage", col("Percentage").cast("double"))
+
+    # Filter for HOUSING and House Type rows
+    df4_house_filtered = df4_melt.filter((col("MAIN CATEGORIES") == "HOUSING") & (col("CATEGORIES") == "House Type"))
+
     # ---------- MERGE DATAFRAMES ----------
-    
-    # MERGE 1: df2 & df6
+
+    # MERGE 1: df4 & df14 --> df4_df14
+    print("\n----------------------------------------------------")
+    print("Merging df4 & df14")
+    merged_df4_df14 = df4_house_filtered.join(
+        df14,
+        df4_house_filtered["REFERENCE"] == df14["Housing Type"],
+        how="left"
+    ).drop("REFERENCE", "MAIN CATEGORIES", "CATEGORIES", "Value", "Housing Type")
+
+    # Calculate Efficiency and Potential Values
+    merged_df4_df14 = merged_df4_df14.withColumn("Efficiency_Value", col("Current Efficiency") * col("Percentage")) \
+                                     .withColumn("Potential_Value", col("Potential Efficiency") * col("Percentage"))
+
+    # Group by Acorn and sum the values
+    final_df4_df14 = merged_df4_df14.groupBy("Acorn").agg(
+        F.sum("Efficiency_Value").alias("Efficiency_Value"),
+        F.sum("Potential_Value").alias("Potential_Value")
+    )
+    final_df4_df14 = final_df4_df14.withColumn("Difference_Value", col("Potential_Value") - col("Efficiency_Value"))
+
+    # Drop unnecessary columns
+    final_df4_df14 = final_df4_df14.drop('Current Efficiency', 'Potential Efficiency')
+
+    # Cast column types
+    final_df4_df14 = final_df4_df14.withColumn("Efficiency_Value", col("Efficiency_Value").cast("double"))
+    final_df4_df14 = final_df4_df14.withColumn("Potential_Value", col("Potential_Value").cast("double"))
+    final_df4_df14 = final_df4_df14.withColumn("Difference_Value", col("Difference_Value").cast("double"))
+
+    print("After merging df4 and df14:")
+    print(f"Columns: {final_df4_df14.columns}")
+    final_df4_df14.show(10)
+
+    # MERGE 2: df2 & df6 --> df2_df6
     print("\n----------------------------------------------------")
     print("Merging df2 & df6:")
     merged_df2_df6 = df2.join(df6, on="LCLid", how="left")
@@ -61,7 +141,7 @@ def main():
     print(f"Columns: {merged_df2_df6.columns}")
     merged_df2_df6.show(10)
 
-    # MERGE 2: df2 & df6 & df10
+    # MERGE 3: df2_df6 & df10 --> df2_df6_df10
     print("\n----------------------------------------------------")
     print("Merging df2 & df6 & df10:")
     merged_df2_df6_df10 = merged_df2_df6.join(df10_reduced, on="Acorn", how="left")
@@ -69,7 +149,7 @@ def main():
     print(f"Columns: {merged_df2_df6_df10.columns}")
     merged_df2_df6_df10.show(10)
     
-    # MERGE 3: df2 & df6 & df10 & df12
+    # MERGE 4: df2_df6_df10 & df12 --> df2_df6_df10_df12
     print("\n----------------------------------------------------")
     print("Merging df2 & df6 & df10 & df12:")
     merged_df2_df6_df10_df12 = merged_df2_df6_df10.join(df12, merged_df2_df6_df10["tstp"] == df12["TariffDateTime"], how="inner")
@@ -78,15 +158,23 @@ def main():
     print(f"Columns: {merged_df2_df6_df10_df12.columns}")
     merged_df2_df6_df10_df12.show(10)
 
+    # MERGE 5: df2_df6_df10_df12 & df4_df14 --> df2_df4_df6_df10_df12_df14
+    print("\n----------------------------------------------------")
+    print("Merging df2 & df6 & df10 & df12 & df4 & df14:")
+    merged_df2_df4_df6_df10_df12_df14 = merged_df2_df6_df10_df12.join(final_df4_df14, on="Acorn", how="left")
+    print("After merging with household demographics info (df4_df14):")
+    print(f"Columns: {merged_df2_df4_df6_df10_df12_df14.columns}")
+    merged_df2_df4_df6_df10_df12_df14.show(10)
+
     print("\n----------------------------------------------------")
     print("Schema of final DataFrame:")
-    merged_df2_df6_df10_df12.printSchema()
+    merged_df2_df4_df6_df10_df12_df14.printSchema()
 
-    print(f"Number of rows: {merged_df2_df6_df10_df12.count()}")
-    merged_df2_df6_df10_df12.show(10)
+    print(f"Number of rows: {merged_df2_df4_df6_df10_df12_df14.count()}")
+    merged_df2_df4_df6_df10_df12_df14.show(10)
 
     # ---------- WRITE THE FINAL DATAFRAME TO S3 AS PARQUET ----------
-    merged_df2_df6_df10_df12.write.mode("overwrite").parquet(f"{S3_OUTPUT_FOLDER}final_q1_df")
+    merged_df2_df4_df6_df10_df12_df14.write.mode("overwrite").parquet(f"{S3_OUTPUT_FOLDER}final_q1_df")
     print("Processed datasets successfully written to S3 as Parquet!")
 
     # Stop Spark session
